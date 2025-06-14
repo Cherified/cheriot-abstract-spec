2fcf9045318e2066f27bfe5690fb4156971495b1
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2025-06-13 21:58:12 -0700

    abstracting permissions and creating the transitive function

--- src/Spec.v
+++ src/Spec.v
@@ -1,3 +1,83 @@
+From Stdlib Require Import List.
+
+Set Primitive Projections.
+
+(* Represents basic permissions *)
+Module Perm.
+  Inductive t :=
+  | Exec
+  | System
+  | Load
+  | Store
+  | Cap
+  | Sealing
+  | Unsealing.
+End Perm.
+
+(* Represents disjoint semantics regions of memory *)
+Inductive Region :=
+| Stack
+| Global.
+
+(* Represents Call and Return sentries *)
+Inductive Sentry :=
+| UnsealedJump
+| CallEnableInterrupt
+| CallDisableInterrupt
+| CallInheritInterrupt
+| RetEnableInterrupt
+| RetDisableInterrupt.
+
+Section Machine.
+  Variable Value: Type.
+  Variable Addr: Type.
+  Variable Key: Type.
+
+  Record Cap := {
+      capSealed: option Key; (* Whether a cap is sealed, and the sealing key *)
+      capPerms: list Perm.t;
+      capThisRegion: Region; (* The provenant region from which this cap was derived *)
+      capStRegions: list Region; (* The regions where this cap can be stored *)
+      capSentry: Sentry;
+      capSealingKeys: list Key; (* List of sealing keys owned by this cap *)
+      capUnsealingKeys: list Key; (* List of unsealing keys owned by this cap *)
+      capAddrs: list Addr; (* List of addresses representing this cap's bounds *)
+      capTransKeepPerms: list Perm.t; (* Permissions to be the only ones kept on load using this cap *)
+      capTransKeepStRegions: list Region (* Regions-where-this-cap-can-be-store to be the only ones kept
+                                            on load using this cap *)
+    }.
+
+  (* When a cap y is loaded using a cap x, then the transitiveRm* of x comes into play *)
+  Record fixLoadedCap (x: Cap) (y: Cap) (z: Cap) : Prop := {
+      sealedEq: z.(capSealed) = y.(capSealed);
+      capPermsEq: forall p, In p z.(capPerms) -> (In p x.(capTransKeepPerms) /\ In p y.(capPerms));
+      capThisRegionEq: z.(capThisRegion) = y.(capThisRegion);
+      capStRegionsEq: forall r, In r z.(capStRegions) -> (In r x.(capStRegions) /\ In r y.(capStRegions));
+      capSentryEq: z.(capSentry) = y.(capSentry);
+      capSealingKeysEq: z.(capSealingKeys) = y.(capSealingKeys);
+      capUnsealingKeysEq: z.(capUnsealingKeys) = y.(capUnsealingKeys);
+      capAddrsEq: z.(capAddrs) = y.(capAddrs);
+      capTransKeepPermsRel: forall p, In p z.(capTransKeepPerms) ->
+                                      (In p x.(capTransKeepPerms) /\ In p y.(capTransKeepPerms));
+      capTransKeepStRegionsRel: forall r, In r z.(capTransKeepStRegions) ->
+                                          (In r x.(capTransKeepStRegions) /\ In r y.(capTransKeepStRegions)) }.
+
+  Variable Memory: Addr -> (Value * list Cap).
+
+  (* Transitively reachable cap with permissions removed according to transitive properties *)
+  Inductive TransCapCap: Cap -> Cap -> Prop :=
+  | Refl (c: Cap): TransCapCap c c
+  | Step (c: Cap) (prev: Cap) (trans: TransCapCap c prev)
+      (next: Cap) (inBounds: exists a, In a prev.(capAddrs) /\ In next (snd (Memory a)))
+      (fixedNext: Cap) (correct: fixLoadedCap prev next fixedNext):
+    TransCapCap c next.
+
+  (* Transitively reachable addr listed with permissions (using the previous relation on Cap x Cap) *)
+  Inductive TransCapAddr: Cap -> Addr -> list Perm.t -> Prop :=
+  | HasAddr (c1 c2: Cap) (tr: TransCapCap c1 c2) (a: Addr) (ina: In a c2.(capAddrs))
+      perms (permsEq: perms = c2.(capPerms)): TransCapAddr c1 a perms.
+End Machine.
+
 Require Import coqutil.Map.Interface.
 Require Import coqutil.Byte.
 Require Import coqutil.Word.Interface.
